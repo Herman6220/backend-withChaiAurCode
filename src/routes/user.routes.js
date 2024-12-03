53b840da14d3c128133ee6a345a00f459e80e08f
Sankalp <sankalpkrpoddar000@gmail.com>
2024-12-03 17:08:37 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+   registerUser, 
+   loginUser, 
+   logoutUser, 
+   refreshAccessToken, 
+   changeCurrentPassword, 
+   getCurrentUser, 
+   updateAccountDetails,
+   updateUserAvatar, 
+   updateUserCoverImage, 
+   getUserChannelProfile, 
+   getWatchHistory 
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJwt} from "../middlewares/auth.middleware.js"
 
@@ -25,6 +37,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single(""/coverImage), updateUserCoverImage)
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
 //So here we can also push two methods in one route, as we needed to verify before logging Out the user, That's why we used next() method in verifyJwt so that after it gets executed, next method will be executed.
 
 export default router
